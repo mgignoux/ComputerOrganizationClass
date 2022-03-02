ccdd91d1d591ca268e4bb5ae34d7b84241e37630
mgignoux <77902768+mgignoux@users.noreply.github.com>
2022-03-02 18:01:12 +0100

    Add files via upload

--- hello.s
+++ hello.s
@@ -1,12 +1,13 @@
-.global main
+.global main                    # makes the subroutine main visible
 
-mystring: .asciz "Names: Madeleine Gignoux, Sonia Welinder \nStudent Numbers: 2695118, 2706057\nAssignment Name: hello\n"
+mystring: .asciz "Names: Madeleine Gignoux, Sonia Welinder \nStudent Numbers: 2695118, 2706057\nAssignment Name: hello\n" 
+                                # declares mystring as an asciz string
 
 main: 
-    movq    $0, %rax
-    movq    $mystring, %rdi
-    call    printf
+    movq    $0, %rax            # first argument of printf, sets no vector registers for use in printf
+    movq    $mystring, %rdi     # second argument of printf, sets the adress of ths string in RDI
+    call    printf              # calls printf
 
 end:
-    mov$0,  %rdi
-    call    exit
+    mov     $0,  %rdi           # first argument of exit, sets the exit code to 0
+    call    exit                # calls exit
--- inout.s
+++ inout.s
@@ -1,33 +1,33 @@
-.global main
+.global main                                    # makes the subroutine main visible
 
-formatstr: .asciz "%ld"
-mynumberoutput: .asciz "The result is: %ld \n"
-inputprompt: .asciz "Please enter a number: \n"
+formatstr: .asciz "%ld"                         # formats the output number as .asciz
+mynumberoutput: .asciz "The result is: %ld \n"  # formats the result
+inputprompt: .asciz "Please enter a number: \n" # formats the input prompt
 
 main: 
-    movq    $0, %rax
-    movq    $inputprompt, %rdi
-    call    printf
-    call    inout
-    call    end
+    movq    $0, %rax                            # first argument of printf, sets no vector registers for use in printf                     
+    movq    $inputprompt, %rdi                  # second argument of printf, sets the adress of ths string in RDI
+    call    printf                              # calls printf
+    call    inout                               # calls the subroutine inout
+    call    end                                 # calls the end subroutine when all other computations are done
 
 inout: 
-    pushq   %rbp
-    movq    %rsp, %rbp
-    subq    $8, %rsp
-    leaq    -8(%rbp), %rsi
-    movq    $formatstr, %rdi
-    movq    $0, %rax
-    call    scanf
-    incq    -8(%rbp)
-    movq    $0, %rax
-    movq    $mynumberoutput, %rdi
-    movq    -8(%rbp), %rsi
-    call    printf
-    movq    %rbp, %rsp
-    popq    %rbp
-    ret
+    pushq   %rbp                                # pushes the base pointer over
+    movq    %rsp, %rbp                          # moves the RSP to the base pointer
+    subq    $8, %rsp                            # reserves stack space for the variable
+    leaq    -8(%rbp), %rsi                      # loads the value of the stack adress in RSI (ask ta)
+    movq    $formatstr, %rdi                    # loads the value of formatstr for the result of the scan
+    movq    $0, %rax                            # sets no vector registers for use
+    call    scanf                               # calls scanf
+    incq    -8(%rbp)                            # ask ta why we do this
+    movq    $0, %rax                            # first argument of prinft, sets no vector registers for use
+    movq    $mynumberoutput, %rdi               # second argument of printf, loads the value mynumberoutput into rdi
+    movq    -8(%rbp), %rsi                      # third argument of printf, sets the adress of the string (down 8 from the base pointer) in RSI
+    call    printf                              # calls printf
+    movq    %rbp, %rsp                          # moves the base pointer to RSP in preparation for exiting the function
+    popq    %rbp                                # pops the base pointer back
+    ret                                         # returns out of the subroutine
     
 end:
-    mov     $0, %rdi
-    call    exit
+    mov     $0, %rdi                            # sets the exit code in rdi
+    call    exit                                # calls exit
